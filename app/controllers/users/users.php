618281c51a7a2513b913802d198a2a85f05f8faf
vishal gupta <vishalguptagic@gmail.com>
2020-12-17 20:00:43 +0530

    added register logic

--- app/controllers/users/users.php
+++ app/controllers/users/users.php
@@ -1,35 +1,57 @@
 <?php
+session_start();
 
 include_once("../../config/database.php");
 
 $method = @$_SERVER['REQUEST_METHOD'];
-$action = @$_REQUEST['submit'];
+$action = @$_POST['submit']; // through form submit
 $method = strtolower($method);
 $action = strtolower($action);
 
-// echo "$method => $action";die;
+//echo "$method => $action";die;
 
 if($_SERVER['QUERY_STRING']) {
-    $action = $_SERVER['QUERY_STRING'];
+    $action = $_SERVER['QUERY_STRING']; // through get call
 }
 
-if($method === 'get' && $action === ''){
-    login($method, $action);
-}else if($method === 'get' && $action === 'register'){
-    register($method, $action);
-}else if($method === 'post' && $action === 'register'){
-    register($method, $action);
+//echo "<pre>"; print_r($_SESSION);die;
+if(!registered()){
+    if($action !== 'login'){
+        $action = 'register';
+        register($method, $action);
+    }
+} else {
+    die("user has been registed");
 }
+
+if(!isLoggedIn()){
+    if($action !== 'register') {
+        $action = 'login';
+        //$method = 'get';
+        login($method, $action);
+    }
+}else{
+    die("User has been loggedin");
+}
+
+if($method === 'get' && ($action === '' || $action === 'login')){
+    login($method, $action);
+}else if(($method === 'get' || $method === 'post') && $action === 'register'){
+    register($method, $action);
+}/*else if($method === 'post' && $action === 'register'){
+    register($method, $action);
+}*/
 
 function login($method, $action) {
     if($method === 'get' && ($action === 'login' || $action === '')) {
-      //  echo getcwd();die;
         include_once("../../views/users/login-view.php");
         exit();
     }else if($method === 'post' && $action === 'login') {
-        // Logic for login
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $_SESSION['username'] = $username;
+        die("Successfully loggedin.");
     }
-
 }
 
 
@@ -39,14 +61,41 @@ function register($method, $action){
         exit();
     }else if($method === 'post' && $action === 'register') {
         // Logic for registration
-
+     $username = $_POST['username'];
+     $firstname = $_POST['firstname'];
+     $lastname = $_POST['lastname'];
+     $email = $_POST['email'];
+     $password = $_POST['password'];
+     $confirm_password = $_POST['confirm_password'];
+     $mobile = $_POST['mobile'];
+     
+     $_SESSION['username'] = $username;
+     $_SESSION['firstname'] = $firstname;
+     $_SESSION['lastname'] = $lastname;
+     $_SESSION['email'] = $email;
+     $_SESSION['password'] = $password;
+     $_SESSION['confirm_password'] = $confirm_password;
+     $_SESSION['mobile'] = $mobile;
+     $_SESSION['city'] = $city;
+     $_SESSION['address'] = $address;
         echo "<pre>"; print_r($_REQUEST);die;
     }
 }
 
-
-function isLoggedIn($method, $action) {
-    if($method === '')
-
+function registered(){
+    $status = false;
+    if(@$_SESSION['username']) {
+        $status = true;
+    }
+    return $status;
 }
-?>
+
+
+function isLoggedIn() {
+    $status = false;
+    if(@$_SESSION['username']) {
+        $status = true;
+    }
+    return $status;
+}
+  ?>
