35887b73e0442e2689255f3c2b55fda73696f813
vishal gupta <vishalguptagic@gmail.com>
2020-12-17 20:02:58 +0530

    change container size

--- app/views/users/footer.php
+++ app/views/users/footer.php
@@ -1,6 +1,6 @@
 <br>
 <footer class="footer mt-auto mt-4 bg-light">
-    <div class="container-fluid bg-dark">
+    <div class="container-fluid-lg bg-dark">
         <h5 class="text-light text-center">copy right 2020</h5>
 
     </div>
