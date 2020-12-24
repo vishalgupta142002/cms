73612c2bcb770530b7381af55f6af315db88041c
vishal gupta <vishalguptagic@gmail.com>
2020-12-24 20:28:00 +0530

    change form

--- app/views/users/registration-view.php
+++ app/views/users/registration-view.php
@@ -1,12 +1,3 @@
-<?php
- require_once ('header.php');
-session_start();
-
-$token = (rand(10,10000000000));
-$_SESSION['token'] = $token;
-?>
-
-
 <!doctype html>
 <html lang="en">
 
@@ -16,14 +7,14 @@ $_SESSION['token'] = $token;
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.79.0">
-    <title>register_view</title>
+    <title>Checkout example · Bootstrap v5.0</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/checkout/">
 
 
 
     <!-- Bootstrap core CSS -->
-    <link href="../../../assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
 
     <style>
     .bd-placeholder-img {
@@ -43,43 +34,29 @@ $_SESSION['token'] = $token;
 
 
     <!-- Custom styles for this template -->
-    <link href="form-validation.css" rel="stylesheet">
+    <link href="app/views/users/form-validation.css" rel="stylesheet">
 </head>
 
-<body class="bg-light ">
+<body class="bg-light">
 
-    <div class="container align-items: center  display: flex;">
+    <div class="container">
         <main>
-
             <div class="py-5 text-center">
-                <img class="d-block mx-auto mb-3 mt-3" src="../../../assets/image/logo.png" alt="" width="72"
+                <img class="d-block mx-auto mb-4" src="assets/images/brand/bootstrap-logo.svg" alt="" width="72"
                     height="57">
                 <h2>Registration form</h2>
                 <p class="lead"></p>
             </div>
 
             <div class="row g-3">
-                <div class="col-md-6">
+                <div class="col-md-7 col-lg-8">
                     <h4 class="mb-3">Student Details</h4>
-                    <form class="needs-validation" novalidate action="../../controllers/users/register.php"
-                        method="post">
+                    <form class="needs-validation" novalidate action="?module=users&action=register" method="post">
                         <div class="row g-3">
-
-                            <div class="col-12">
-                                <label for="username" class="form-label">Username</label>
-                                <div class="input-group">
-                                    <!-- <span class="input-group-text">@</span> -->
-                                    <input type="text" class="form-control" name="username" id="username"
-                                        placeholder="Username" required>
-                                    <div class="invalid-feedback">
-                                        Your username is required.
-                                    </div>
-                                </div>
-                            </div>
                             <div class="col-sm-6">
-                                <label for="firstName" class="form-label">First name</label>
-                                <input type="text" class="form-control" name="firstname" id="firstname"
-                                    placeholder="Enter FirstName" value="" required maxlength="10">
+                                <label for="first_name" class="form-label">First name</label>
+                                <input type="text" class="form-control" name="first_name" id="first_name" placeholder=""
+                                    value="" required maxlength="10">
                                 <div class="invalid-feedback">
                                     Valid first name is required.
                                 </div>
@@ -87,104 +64,92 @@ $_SESSION['token'] = $token;
                             </div>
 
                             <div class="col-sm-6">
-                                <label for="lastName" class="form-label">Last name</label>
-                                <input type="text" class="form-control" name="lastname" id="lastname"
-                                    placeholder="Enter Last Name" value="" required>
+                                <label for="last_name" class="form-label">Last name</label>
+                                <input type="text" class="form-control" name="last_name" id="last_name" placeholder=""
+                                    value="" required>
                                 <div class="invalid-feedback">
                                     Valid last name is required.
                                 </div>
                             </div>
 
+                            <div class="col-12">
+                                <label for="user_name" class="form-label">Username</label>
+                                <div class="input-group">
+                                    <span class="input-group-text">@</span>
+                                    <input type="text" class="form-control" name="user_name" id="user_name"
+                                        placeholder="user_name" required>
+                                    <div class="invalid-feedback">
+                                        Your username is required.
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <label for="password" class="form-label">Password</label>
+                                <div class="input-group">
+                                    <span class="input-group-text">@</span>
+                                    <input type="password" class="form-control" name="password" id="password"
+                                        placeholder="password" required>
+                                    <div class="invalid-feedback">
+                                        Your password is required.
+                                    </div>
+                                </div>
+                            </div>
 
                             <div class="col-12">
                                 <label for="email" class="form-label">Email <span class="text-muted"></span></label>
                                 <input type="email" class="form-control" name="email" id="email"
                                     placeholder="you@example.com">
                                 <div class="invalid-feedback">
-                                    Please enter a valid email address .
+                                    Please enter a valid email address.
                                 </div>
                             </div>
-                            <div class="col-12">
-                                <label for="password" class="form-label">Password <span
-                                        class="text-muted"></span></label>
-                                <input type="password" class="form-control" name="password" id="password"
-                                    placeholder="please enter vaild password">
-                                <!-- <div class="invalid-feedback"> -->
-                                Please enter a valid password.
-                                <!-- </div> -->
-                            </div>
-
-                        </div>
-                        <div class="col-12">
-                            <label for="password" class="form-label">confirm Password <span
-                                    class="text-muted"></span></label>
-                            <input type="password" class="form-control" name="confirm_password" id="confirm_password"
-                                placeholder="please enter same confirm Password ">
-                            <!-- <div class="invalid-feedback"> -->
-                            Please enter a valid confirm Password.
-                            <!-- </div> -->
-                        </div>
-
-                        <div class="col-12">
-                            <label for="gender" class="form-label">Gender <span class="text-muted"></span></label>
-                            <div class="form-check">
-                                <input id="male" name="gender" value="male" type="radio" class="form-check-input">
-                                <label class="form-check-label" for="credit">Male</label>
-                            </div>
-                            <div class="form-check">
-                                <input id="female" name="gender" value="female" type="radio" class="form-check-input"
-                                    required>
-                                <label class="form-check-label" for="debit">Female</label>
-                            </div>
 
                             <div class="col-12">
-                                <label for="dob" class="form-label">DOB <span class="text-muted"></span></label>
-                                <input type="date" class="form-control" name="dob" id="dob" placeholder="" required>
+                                <label for="address" class="form-label">Address</label>
+                                <input type="text" class="form-control" name="address" id="address"
+                                    placeholder="1234 Main St">
                                 <div class="invalid-feedback">
-                                    Please enter your Date oF Brith.
+                                    Please enter your address.
                                 </div>
                             </div>
+
                             <div class="col-12">
-                                <label for="number" class="form-label">Mobile <span class="text-muted"></span></label>
+                                <label for="Mobile" class="form-label">Mobile <span class="text-muted"></span></label>
                                 <input type="number" class="form-control" name="mobile" id="mobile"
-                                    placeholder="Enter vaild Mobile Number" required>
-                                <div class="invalid-feedback">
-                                    Please enter your Mobile Number.
-                                </div>
-
+                                    placeholder="mobile number">
                             </div>
-                            <div class="col-12">
-                                <label for="resume" class="form-label">Resume <span class="text-muted"></span></label>
-                                <input type="file" class="form-control" name="resume" id="resume" required>
-                                <div class="invalid-feedback">
-                                    Please Choose your Resume.
-                                </div>
 
-                            </div>
                             <div class="col-md-5">
                                 <label for="country" class="form-label">Country</label>
-                                <select class="form-select" id="country" name="country" required>
-                                    <option value="">Choose Country</option>
-                                    <option>India</option>
-                                    <option>Nepal</option>
+                                <select class="form-select" name="country" id="country">
+                                    <option value="">Choose</option>
+                                    <option value="india">india</option>
+                                    <option value="japan">japan</option>
                                 </select>
                                 <div class="invalid-feedback">
                                     Please select a valid country.
                                 </div>
                             </div>
 
+                            <div class="col-md-4">
+                                <label for="state" class="form-label">State</label>
+                                <select class="form-select" name="state" id="state">
+                                    <option value="">Choose...</option>
+                                    <option value="up">up</option>
+                                    <option value="mp">mp</option>
 
-                            <div class="col-12">
-                                <label for="address2" class="form-label">city <span class="text-muted"></span></label>
-                                <input type="text" class="form-control" name="city" id="city"
-                                    placeholder=" Enter vaild City ">
-                            </div>
-                            <div class="col-12">
-                                <label for="address" class="form-label">Address</label>
-                                <input type="text" class="form-control" name="address" id="address"
-                                    placeholder="Enter vaild Address" required>
+                                </select>
                                 <div class="invalid-feedback">
-                                    Please enter your address.
+                                    Please provide a valid state.
+                                </div>
+                            </div>
+
+                            <div class="col-md-3">
+                                <label for="zip" class="form-label">Zip</label>
+                                <input type="text" class="form-control" name="zip" id="zip" placeholder="">
+                                <div class="invalid-feedback">
+                                    Zip code required.
                                 </div>
                             </div>
                         </div>
@@ -193,30 +158,94 @@ $_SESSION['token'] = $token;
 
                         <div class="form-check">
                             <input type="checkbox" class="form-check-input" name="course[]" value="btech" id="course">
-                            <label class="form-check-label" for="course">BTECH
+                            <label class="form-check-label" for="same_address">btech
                             </label>
                         </div>
 
                         <div class="form-check">
                             <input type="checkbox" class="form-check-input" name="course[]" value="bca" id="course">
-                            <label class="form-check-label" for="course">BCA</label>
+                            <label class="form-check-label" for="save-info">bca</label>
                         </div>
+
                         <hr class="my-4">
 
-                        <button class="w-100 btn btn-primary btn-lg" type="submit">Register</button>
+                        <h4 class="mb-3">gender</h4>
+
+                        <div class="my-3">
+                            <div class="form-check">
+                                <input id="gender" name="gender" type="radio" value="male" class="form-check-input">
+                                <label class="form-check-label" for="credit">male</label>
+                            </div>
+                            <div class="form-check">
+                                <input id="gender" name="gender" type="radio" value="female" class="form-check-input">
+                                <label class="form-check-label" for="debit">female</label>
+                            </div>
+                            <!-- <div class="form-check">
+                                <input id="paypal" name="paymentMethod" type="radio" class="form-check-input">
+                                <label class="form-check-label" for="paypal">PayPal</label>
+                            </div> -->
+                        </div>
+
+                        <!--   <div class="row gy-3">
+                            <div class="col-md-6">
+                                <label for="cc-name" class="form-label">Name on card</label>
+                                <input type="text" class="form-control" name="cc-name" id="cc-name" placeholder="">
+                                <small class="text-muted">Full name as displayed on card</small>
+                                <div class="invalid-feedback">
+                                    Name on card is required
+                                </div>
+                            </div>
+
+                             <div class="col-md-6">
+                                <label for="cc-number" class="form-label">Credit card number</label>
+                                <input type="text" class="form-control" name="cc-number" id="cc-number" placeholder="">
+                                <div class="invalid-feedback">
+                                    Credit card number is required
+                                </div>
+                            </div> -->
+
+                        <!-- <div class="col-md-3">
+                                <label for="cc-expiration" class="form-label">Expiration</label>
+                                <input type="text" class="form-control" name="cc-expiration" id="cc-expiration"
+                                    placeholder="">
+                                <div class="invalid-feedback">
+                                    Expiration date required
+                                </div>
+                            </div> -->
+
+                        <!-- <div class="col-md-3">
+                                <label for="cc-cvv" class="form-label">CVV</label>
+                                <input type="text" class="form-control" name="cc-cvv" id="cc-cvv" placeholder="">
+                                <div class="invalid-feedback">
+                                    Security code required
+                                </div>
+                            </div> 
+                        </div>-->
+
+                        <hr class="my-4">
+
+                        <button class="w-100 btn btn-primary btn-lg" type="submit" name="submit"
+                            value="register">Register</button>
+                        <a href="?module=users&action=login">Login</a>
                     </form>
                 </div>
             </div>
         </main>
+
+        <footer class="my-5 pt-5 text-muted text-center text-small">
+            <p class="mb-1">&copy; 2017–2020 Company Name</p>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Privacy</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Support</a></li>
+            </ul>
+        </footer>
     </div>
 
 
-    <script src="../../../assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 
-    <script src="form-validation.js"></script>
+    <script src="app/views/users/form-validation.js"></script>
 </body>
 
 </html>
-<?php
-require_once 'footer.php';
-?>
